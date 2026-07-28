ce5fa8abaaf27de0eb324621242d5bf23bfdb01b
AmurKhoyetsyan <javanewss@gmail.com>
2026-07-28 16:42:29 +0400

    remove other functional

--- routers/imgvid/routes/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Route sub-package for the image-video editor.
-
-Importing this package does not register any routes.  Callers should import
-the individual sub-modules and include their routers into the parent router.
-"""
